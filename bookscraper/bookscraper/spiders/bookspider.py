b58596c4913d3b55f9ae3c6ff79305f8cef14a1b
Amalie <amaliegay@outlook.com>
2023-11-27 23:31:39 +0800

    != is not is not

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,23 +1,23 @@
 import scrapy
-from bookspider.items import BookItem
+from bookscraper.items import BookItem
 
 
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["www.jjwxc.net"]
     start_urls = [
-        "https://www.jjwxc.net/bookbase.php?fw0=0&fbsj0=0&novelbefavoritedcount0=0&yc1=1&xx3=3&mainview0=0&sd0=0&lx1=1&bq=&removebq=&isfinish=0&collectiontypes=ors&searchkeywords=&sortType=1&page=1"
+        "https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&isfinish=0&collectiontypes=&searchkeywords=&page=1&sortType=2"
     ]
 
     def parse(self, response):
-        # fetch('https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&sortType=1&page=1')
+        # fetch('https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&isfinish=0&collectiontypes=&searchkeywords=&page=1&sortType=2')
         books = response.css("table.cytable tbody tr")
 
         for book in books:
-            if book.css("td a") is not []:
+            if book.css("td a") != []:
                 relative_url = book.css("td a")[1].attrib["href"]
                 url = "https://www.jjwxc.net/" + relative_url
-				yield response.follow(url, callback=self.parse_book_page)
+                yield response.follow(url, callback=self.parse_book_page)
 
         page_field = "page="
         current_page_index = int(
@@ -32,9 +32,11 @@ class BooksSpider(scrapy.Spider):
         if next_page_index <= 10:
             yield response.follow(next_page_url, callback=self.parse)
 
-def parse(self, response):
-	book_item = BookItem()
-	book_item["url"] = response.url
-	book_item["title"] = response.xpath("span[@itemprop='articleSelection']/text()").get()
-	book_item["description"] = response.xpath("div[@id='novelintro']/text()").get()
-	yield book_item
+    def parse_book_page(self, response):
+        book_item = BookItem()
+        book_item["url"] = response.url
+        book_item["title"] = response.css("span.bigtext h1 span::text").get()
+        book_item["description"] = response.xpath(
+            "//div[@id='novelintro']/text()"
+        ).getall()
+        yield book_item
