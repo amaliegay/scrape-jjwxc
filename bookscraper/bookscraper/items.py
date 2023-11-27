368d36858b017a69b792f4b414056b4ac5497b0d
Amalie <amaliegay@outlook.com>
2023-11-27 19:36:05 +0800

    Update items.py:bookitem

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -10,3 +10,8 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    description = scrapy.Field()
