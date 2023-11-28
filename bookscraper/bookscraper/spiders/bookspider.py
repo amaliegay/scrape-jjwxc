4767bb203995a242b56420af40a32fe837e81366
Amalie <amaliegay@outlook.com>
2023-11-28 19:36:30 +0800

    book_item["description"] = description

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -6,10 +6,8 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["www.jjwxc.net"]
 
-	url_no_index = "https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&isfinish=0&collectiontypes=&searchkeywords=&sortType=2&page="
-    start_urls = [
-        url + "1"
-    ]
+    url_no_index = "https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&isfinish=0&collectiontypes=&searchkeywords=&sortType=2&page="
+    start_urls = [url_no_index + "1"]
 
     def parse(self, response):
         # fetch('https://www.jjwxc.net/bookbase.php?yc=1&xx=3&lx=1&isfinish=0&collectiontypes=&searchkeywords=&page=1&sortType=2')
@@ -26,10 +24,7 @@ class BooksSpider(scrapy.Spider):
             response.url[response.url.find(page_field) + len(page_field)]
         )
         next_page_index = current_page_index + 1
-        next_page_url = (
-            url_no_index
-            + str(next_page_index)
-        )
+        next_page_url = self.url_no_index + str(next_page_index)
 
         if next_page_index <= 10:
             yield response.follow(next_page_url, callback=self.parse)
@@ -38,8 +33,9 @@ class BooksSpider(scrapy.Spider):
         book_item = BookItem()
         book_item["url"] = response.url
         book_item["title"] = response.css("span.bigtext h1 span::text").get()
-		description = response.xpath(
-            "//div[@id='novelintro']/text()"
-        ).getall() or response.xpath("//div[@id='novelintro']/font/text()").getall()
-        book_item["description"] = 
+        description = (
+            response.xpath("//div[@id='novelintro']/text()").getall()
+            or response.xpath("//div[@id='novelintro']/font/text()").getall()
+        )
+        book_item["description"] = description
         yield book_item
